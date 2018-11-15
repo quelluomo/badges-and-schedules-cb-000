8636f2c1865f9f562abf79a989f4deb03340d8b0
Matteo Muscas <thatman.j@gmail.com>
2018-11-15 10:14:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,11 @@ end
 def batch_badge_creator(name)
   badge_array = []
   name.each do |guest|
-  badge_array << badge_maker
+  badge_array << "Hello, my name is #{guest}."
   end
   badge_array
 end
+ 
+def assign_rooms
+
+end 
